4cabc90c8e4d332f11f4fb5507eba48af57da8c3
X1NK3R <av3stube@gmail.com>
2018-08-28 10:23:52 +0200

    files

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+char * test(size_t  m, size_t n, char in[m][n], char * result) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            result[i * m + j] = in[i][j];
+        }
+    }
+}
+
+int main() {
+    char input[2][3] = {
+        {'1', '2', '3'},
+        {'4', '5', '6'}
+    };
+
+    int row = sizeof(input) / sizeof(input[0]);
+    int column = sizeof(input[0]) / sizeof(input[0][0]);
+
+    char result[row * column];
+
+    test(sizeof(input) / sizeof(input[0]), sizeof(input[0]) / sizeof(input[0][0]), input, result);
+
+    for (int i = 0 ; i < row * column; i++) {
+        printf("%d\n", result[i]);
+    }
+
+    return 0;
+}
